871a37ad5e7b810d922ef6676972a8b816fc0685
Maximo Bautista <max.bautista97@gmail.com>
2019-02-20 18:43:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  
+  get '/' do
+    "Hello, World!"
+  end
+  
+  get '/name' do
+    "My name is Maximo"
+  end
+  
+  get '/hometown' do
+    "My hometown is La Romana R.D."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is The Time"
+  end
+
 end
